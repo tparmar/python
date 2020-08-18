3ab96a3356051298f70512afd76c541236e90753
Teerthraj Parmar <68967593+tparmar@users.noreply.github.com>
2020-08-18 12:02:08 -0400

    DNA Case Studies
    This my data and code from my DNA case study.

---
+++ DNA_Case_Studies/dna_translate.py
@@ -0,0 +1,45 @@
+table = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+}
+def read_seq(inputfile):
+    with open(inputfile,"r") as f:
+        seq = f.read()
+    seq.replace("\n", "")
+    seq.replace("\r", "")
+    return seq
+prt = read_seq("protein.txt")
+dna = read_seq("DNA_sequence.txt")
+
+protein = ""
+def translate(seq):
+    protein = ""
+    if len(seq)%3 == 0:
+        for i in range(0,len(seq),3):
+            codon = seq[i:i+3]
+            protein += table[codon]
+    return protein
+
+print(translate(dna[20:938])[:-1] == translate(dna[20:935]))
+#pseudocode:
+    #check thaat the sequence length is divisible by 3
+        #if true, then we can loop over the sequence
+            #extract a single codon
+            #look up the codon and store the result
+
+
+
